6b2ba26b563f875855e1221dda2ad9eded9778c1
Yasser Dahab <y@yasserdahab.com>
2014-01-01 19:50:32 -0700

    URI change

--- lib/omniauth/strategies/gca.rb
+++ lib/omniauth/strategies/gca.rb
@@ -3,7 +3,7 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Gca < OmniAuth::Strategies::OAuth2
-      GCA_SSO_PROVIDER_URL = ENV["GCA_SSO_GATEWAY"] || "http://localhost:3000"
+      GCA_SSO_PROVIDER_URL = ENV["GCA_SSO_GATEWAY"]
         
       class << self
         attr_accessor :gca_sso_token
