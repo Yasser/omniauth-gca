cf03af5f7ac25d9b24a2da1008e46c8c2389eb44
Yasser Dahab <y@yasserdahab.com>
2014-05-09 20:59:41 -0600

    Debug

--- lib/omniauth/strategies/gca.rb
+++ lib/omniauth/strategies/gca.rb
@@ -55,6 +55,7 @@ class GcaSsoApi
     @client = OAuth2::Client.new(ENV["GCA_SSO_APP_ID"], ENV["GCA_SSO_APP_SECRET"], site: @provider_host, :raise_errors => false)
     @token = @client.client_credentials.get_token
     @user_token = user_token
+    logger.debug("********** GcaSsoApi: #{@user_token}")
     @response = nil
   end
   
@@ -68,8 +69,10 @@ class GcaSsoApi
   
   def post
     if @user_token
+      logger.debug("********** GcaSsoApi: Posting with usertoken...")
       @response = RestClient.post @request_uri, {params: @params}, { 'Authorization' => "Bearer #{@user_token}" }
     else
+      logger.debug("********** GcaSsoApi: Posting from client...")
       @response = token.post(@request_uri, params: @params)
     end
   end
