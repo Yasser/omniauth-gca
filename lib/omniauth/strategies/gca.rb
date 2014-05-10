6b47ff6422527f55f019de3a5d6c6bf17fb1e20b
Yasser Dahab <y@yasserdahab.com>
2014-05-09 21:37:00 -0600

    Genius

--- lib/omniauth/strategies/gca.rb
+++ lib/omniauth/strategies/gca.rb
@@ -64,7 +64,7 @@ class GcaSsoApi
   
   def get
     if @user_token
-      @response[@request_uri] = @client.get :Authorization => "Bearer #{@user_token}"
+      @response = @client[@request_uri].get :Authorization => "Bearer #{@user_token}"
     else
       @response = token.get(@request_uri)
     end
@@ -72,7 +72,7 @@ class GcaSsoApi
   
   def post
     if @user_token
-      @response[@request_uri] = @client.post :params => @params, :Authorization => "Bearer #{@user_token}"
+      @response = @client[@request_uri].post :params => @params, :Authorization => "Bearer #{@user_token}"
     else
       @response = token.post(@request_uri, params: @params)
     end
