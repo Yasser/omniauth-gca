d89c3a8b65983ab92e3d840d0f045c3bf71b801d
Yasser Dahab <y@yasserdahab.com>
2015-03-27 15:58:53 -0600

    This might break shit.

--- app/controllers/gca_sso_client/application_controller.rb
+++ app/controllers/gca_sso_client/application_controller.rb
@@ -1,5 +1,5 @@
 module GcaSsoClient
-  class ApplicationController < ActionController::Base
+  class ApplicationController < Rails::Application::ApplicationController
 
     def expire_session
       redirect_to signout_url, _method: :delete, notice: "Your session has ended due to inactivity. Please <a href=\"#{signin_url}\">sign in</a> again.".html_safe
--- lib/omniauth-gca/version.rb
+++ lib/omniauth-gca/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module Gca
-    VERSION = "0.1.18"
+    VERSION = "0.1.19"
   end
 end
