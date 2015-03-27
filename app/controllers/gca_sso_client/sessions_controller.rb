4d52a4addfe81743f0429a15e3ed8a09ded88e98
Yasser Dahab <y@yasserdahab.com>
2015-03-27 13:44:46 -0600

    Naming convention

--- app/controllers/gca_sso_client/sessions_controller.rb
+++ app/controllers/gca_sso_client/sessions_controller.rb
@@ -1,5 +1,3 @@
-require_dependency 'gca_sso_client/application_controller'
-
 module GcaSsoClient
   class SessionsController < ApplicationController
     before_action :sync_access_groups, only: [:create]
--- lib/omniauth-gca/version.rb
+++ lib/omniauth-gca/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module Gca
-    VERSION = "0.1.11"
+    VERSION = "0.1.14"
   end
 end
